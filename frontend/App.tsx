858a5ec2fc07123c7609c35df1c6693035ab8f8d
MeherabHossain007 <meherab788@gmail.com>
2023-10-14 10:50:01 +0600

    navigation

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import { NativeBaseProvider, Box } from "native-base";
 import Home from "./src/screens/home";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginPage from "./src/screens/LoginPage";
+
+const stack = createStackNavigator();
+
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Home/>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <stack.Navigator>
+          <stack.Screen name="Home" component={Home}/>
+          <stack.Screen name="Login" component={LoginPage}/>
+        </stack.Navigator>
+      </NativeBaseProvider>
+    </NavigationContainer>
   );
 }
-
--- frontend/src/screens/CounterPage.tsx
+++ frontend/src/screens/CounterPage.tsx
@@ -1,30 +1,43 @@
-import { Button, HStack, View } from "native-base";
+import { Button, HStack, VStack, View } from "native-base";
 import React, { useState } from "react";
 import { Text, Vibration } from "react-native";
 import { StyleSheet } from "react-native";
 
-export default function CounterPage() {
+const CounterPage= (props: any) => {
   const [counter, setCounter] = useState(0);
 
   const increaseHandeler = () => {
     setCounter(counter + 1);
-    Vibration.vibrate(100)
+    Vibration.vibrate(100);
   };
 
   const decreaseHandeler = () => {
     setCounter(counter - 1);
-    Vibration.vibrate(100)
+    Vibration.vibrate(100);
+  };
+
+  const navigationHandeler = () => {
+   props.navigation.navigate("Login");
   };
 
   return (
     <View style={style.mainContainer}>
       <Text style={style.counterContainer}>{counter}</Text>
       <View>
-        <HStack space={3}>
-          <Button onPress={increaseHandeler}>Increase</Button>
-          <Button onPress={decreaseHandeler}>Decrease</Button>
-          <Button onPress={()=>{setCounter(0)}}>Reset</Button>
-        </HStack>
+        <VStack space={3}>
+          <HStack space={3}>
+            <Button onPress={increaseHandeler}>Increase</Button>
+            <Button onPress={decreaseHandeler}>Decrease</Button>
+            <Button
+              onPress={() => {
+                setCounter(0);
+              }}
+            >
+              Reset
+            </Button>
+          </HStack>
+          <Button onPress={navigationHandeler}>Login</Button>
+        </VStack>
       </View>
     </View>
   );
@@ -42,3 +55,5 @@ const style = StyleSheet.create({
     color: "red",
   },
 });
+
+export default CounterPage;
